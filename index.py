cc58676e31286845207e888c133b38bae5376e9e
Klemer Monteiro <klemermonteiro93@gmail.com>
2020-09-30 13:33:33 -0300

    topicos_02

---
+++ index.py
@@ -0,0 +1,13 @@
+from MyMongoDatabase import MyMongoDatabase
+from connection import connectToTwitterAPI
+import tweepy
+
+try:
+    myMongo = MyMongoDatabase('modelartopicosweb')
+    api = connectToTwitterAPI()
+    tweetsList = tweepy.Cursor(api.search, q="qualquer texto").items(10)
+    for tweet in tweetsList:
+        myMongo.insert(
+            "tweets", {'author': tweet.author.screen_name, 'text': tweet.text})
+except tweepy.TweepError:
+    print('Problema na conexão com a API')
